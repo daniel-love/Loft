1297b91ba3380fa35ed7ade32d56dda33904a652
Derek Martinez <me@derek-martinez.us>
2014-01-11 13:54:18 -0500

    Tabs

---
+++ js/modules/tabs.jquery.js
@@ -0,0 +1,33 @@
+(function($) {
+	$.fn.tabs = function (options)
+	{
+		var options = $.extend({div: '.tabs'}, options);
+
+		$('ul.tabs').each(function(){
+			var _current, _body, _hrefs;
+
+			_hrefs = $(this).find('a');
+			_current = $(_hrefs.filter('[href="'+location.hash+'"]')[0] || _hrefs[0]);
+
+			_current.addClass('active');
+			_body = $(_current.attr('href'));
+
+			_hrefs.not(_current).each(function (){
+				$($(this).attr('href')).hide();
+			});
+
+			$(this).on('click', 'a', function(_e) {
+				_e.preventDefault();
+				
+				_current.removeClass('active');
+				_body.hide();
+
+				_current = $(this);
+				_body = $($(this).attr('href'));
+
+				_current.addClass('active');
+				_body.show();
+			});
+		});
+	}
+})(jQuery);
