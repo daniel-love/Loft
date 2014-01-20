d83f3e71bee415cc29099e63ad39cf53f3752bf2
Derek Martinez <me@derek-martinez.us>
2014-01-20 03:51:23 -0500

    jQuery plugin update
    Added absolute basics of scroll plugin; rewrote carousel plugin with
    better naming and method.

--- js/modules/carousel.jquery.js
+++ js/modules/carousel.jquery.js
@@ -1,36 +1,32 @@
 (function($) {
+
     $.fn.carousel = function(options) {
-        console.log("Debug: Called carousel()");
+
         var options = $.extend({
-            transition_time: 1000,
-            delay_time: 4000
+            transition: 1000,
+            delay: 4000
         }, options);
 
-        var object = $(this);
-        var step = $('li');
+        var div = $(this);
+        var li = $('li');
 
-        function steps() {
-            return object.find(step);
+        function items() {
+            return div.find(li);
         }
 
-        steps().fadeOut();
-        steps().first().addClass('active');
-        steps().first().fadeIn(options['transition_time']);
+        items().fadeOut();
+        items().first().addClass('active').fadeIn(options['transition']);
 
-        timer = setInterval(function() {
-            var index = object.find('li.active').index();
+        setInterval(function() {
+            var index = div.find('li.active').index();
 
-            steps().eq(index).removeClass('active');
-            steps().eq(index).fadeOut(options['transition_time']);
-
-            if (steps().length == index + 1) index = -1;
-
-            steps().eq(index + 1).fadeIn(options['transition_time']);
-            steps().eq(index + 1).addClass('active');
-
-        }, options['transition_time'] + options['delay_time']);
+            items().eq(index).removeClass('active').fadeOut(options['transition'], function() {
+                if (items().length == index + 1) index = -1;
+                items().eq(index + 1).fadeIn(options['transition'], function() {
+                    items().eq(index + 1).addClass('active');
+                })
+            })
+        }, options['transition'] + options['delay']);
     }
+
 })(jQuery);
-// To-Do
-// Add more methods
-//
---
+++ js/modules/scroll.jquery.js
@@ -0,0 +1,13 @@
+(function($) {
+    $.fn.scrollTo = function(options) {
+
+        var options = $.extend({
+            speed: 2000,
+            ease: 'swing' //Accepts easing such as easein, swing, linear blahj blahblahb!
+        }, options);
+
+        $("html, body").animate({
+                scrollTop: $(this).offset().top
+            },options['speed'],options['ease']);
+    }
+})(jQuery);
